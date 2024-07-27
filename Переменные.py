d5b8adaa69e3bb75abfd54930a0233d13f6a924a
CaptainNemo1993 <mr.tofik1993@mail.ru>
2024-07-28 01:43:06 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+name="Python"
+homework=12
+all_time=1.5
+result=all_time/homework
+print('Курс:',str(name)+', всего задач:'+str(homework)+', затрачено часов:'+str(all_time)+', среднее время выполнения:',result,"часа." )
