10790c6d99276f182b34a6ef9c7eaf5789e9efc7
CaptainNemo1993 <mr.tofik1993@mail.ru>
2024-08-01 03:12:29 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,6 @@
+immutable_var=(1,15,True, 'HELLO', [5, 8, 'game'])
+print(immutable_var)
+#immutable_var[0}=tiger # Данные кортеджа не изменяемы
+mutable_list=[0, 'dance', 'orange', 66, True]
+mutable_list[-1]=False
+print(mutable_list)
